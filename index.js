a4a7eee2f79a240dff06c3afab2ee3bf331245ce
Diego Quintela <diegolucasq@gmail.com>
2021-12-12 20:07:17 -0500

    added functionality to the 'random button'. added new songs

--- index.js
+++ index.js
@@ -39,8 +39,15 @@ const song5 = {
   album: "Nefertiti",
   duration: "7:51",
 };
+const song6 = {
+  id: 6,
+  title: "Bag's Groove (Take 1)",
+  artist: "Miles Davis",
+  album: "Bag's Groove Remastered",
+  duration: "11:16",
+};
 
-const songs = [song1, song2, song3, song4, song5];
+const songs = [song1, song2, song3, song4, song5, song6];
 
 const containerSongs = document.querySelector(".container-test");
 
@@ -50,9 +57,13 @@ const songArtist = document.querySelector(".song-artist");
 const songAlbum = document.querySelector(".song-album");
 const songDuration = document.querySelector(".song-duration");
 
+//action buttons
+const buttonRandom = document.querySelector(".button-random");
+
 containerSongs.innerHTML = "";
 
 // display songs
+
 songs.forEach((song) => {
   const html = `
     <p class="songlist-songs song-id">${song.id}</p>
@@ -64,3 +75,36 @@ songs.forEach((song) => {
 
   containerSongs.insertAdjacentHTML("afterbegin", html);
 });
+
+//buttons functionality
+
+//random (shuffle)
+buttonRandom.addEventListener("click", (e) => {
+  console.log("clicked");
+  //reload page
+
+  // window.location.reload();
+
+  /* Randomize array in-place using Durstenfeld shuffle algorithm */
+  function shuffleArray(songs) {
+    for (let i = array.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
+    }
+  }
+  songs.forEach((song) => {
+    const html = `
+      <p class="songlist-songs song-id">${song.id}</p>
+      <p class="songlist-songs song-title">${song.title}</p>
+      <p class="songlist-songs song-artist">${song.artist}</p>
+      <p class="songlist-songs song-album">${song.album}</p>
+      <p class="songlist-songs song-duration">${song.duration}</p>
+      `;
+
+    containerSongs.insertAdjacentHTML("afterbegin", html);
+  });
+});
+
+// need to add an updateUi function
