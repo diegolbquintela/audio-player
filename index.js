34c48b60a8a2fbbc53a0e375e3a3ff61432ab5ac
Diego Quintela <diegolucasq@gmail.com>
2021-12-14 19:15:06 -0500

    organized folders. added previous/next button functionality. refactored the array of songs

--- index.js
+++ index.js
@@ -1,98 +1,117 @@
 "use strict";
+const songs = [
+  {
+    id: 1,
+    title: "Parker's Mood (New take 1)",
+    artist: "Charlie Parker",
+    album: "Charlie Parker Memorial vol 1",
+    duration: "3:27",
+    name: "jacinto-1",
+  },
+  {
+    id: 2,
+    title: "Just Friends",
+    artist: "Charlie Parker",
+    album: "April in Paris: The Genius of Charlie Parker vol 2",
+    duration: "3:32",
+    name: "jacinto-2",
+  },
+  {
+    id: 3,
+    title: "Kim (alternate take)",
+    artist: "Charlie Parker Quartet",
+    album: "Charlie Parker",
+    duration: "2:58",
+    name: "jacinto-3",
+  },
+  {
+    id: 4,
+    title: "Footprints",
+    artist: "Miles Davis",
+    album: "Miles Smiles",
+    duration: "9:49",
+    name: "",
+  },
+  {
+    id: 5,
+    title: "Nefertiti",
+    artist: "Miles Davis",
+    album: "Nefertiti",
+    duration: "7:51",
+    name: "",
+  },
+  {
+    id: 6,
+    title: "Bag's Groove (Take 1)",
+    artist: "Miles Davis",
+    album: "Bag's Groove Remastered",
+    duration: "11:16",
+    name: "",
+  },
+];
 
-const song1 = {
-  id: 1,
-  title: "Parker's Mood (New take 1)",
-  artist: "Charlie Parker",
-  album: "Charlie Parker Memorial vol 1",
-  duration: "3:27",
+const image = document.querySelector("img");
+const title = document.querySelector("#title");
+const artist = document.querySelector("#artist");
+const music = document.querySelector("audio");
+const previousBtn = document.querySelector("#prev");
+const playBtn = document.querySelector("#play");
+const nextBtn = document.querySelector("#next");
+
+// conditional statement: check if it is playing
+let isPlaying = false;
+
+// play functionality
+const playSong = () => {
+  isPlaying = true;
+  music.play();
 };
 
-const song2 = {
-  id: 2,
-  title: "Just Friends",
-  artist: "Charlie Parker",
-  album: "April in Paris: The Genius of Charlie Parker vol 2",
-  duration: "3:32",
+// pause
+const pauseSong = () => {
+  isPlaying = false;
+  music.pause();
 };
 
-const song3 = {
-  id: 3,
-  title: "Kim (alternate take)",
-  artist: "Charlie Parker Quartet",
-  album: "Charlie Parker",
-  duration: "2:58",
+playBtn.addEventListener("click", () => (isPlaying ? pauseSong() : playSong()));
+
+// update UI
+const loadSongs = (songs) => {
+  title.textContent = songs.title;
+  artist.textContent = songs.artist;
+  music.src = `songs/${songs.name}.mp3`;
+  image.src = `images/${songs.artist}.jpg`;
 };
 
-const song4 = {
-  id: 4,
-  title: "Footprints",
-  artist: "Miles Davis",
-  album: "Miles Smiles",
-  duration: "9:49",
-};
+// current song
+let songIndex = 0;
 
-const song5 = {
-  id: 5,
-  title: "Nefertiti",
-  artist: "Miles Davis",
-  album: "Nefertiti",
-  duration: "7:51",
-};
-const song6 = {
-  id: 6,
-  title: "Bag's Groove (Take 1)",
-  artist: "Miles Davis",
-  album: "Bag's Groove Remastered",
-  duration: "11:16",
-};
+// selecting song to load
+loadSongs(songs[songIndex]);
 
-const songs = [song1, song2, song3, song4, song5, song6];
-
-const containerSongs = document.querySelector(".container-test");
-
-const songId = document.querySelector(".song-id");
-const songTitle = document.querySelector(".song-title");
-const songArtist = document.querySelector(".song-artist");
-const songAlbum = document.querySelector(".song-album");
-const songDuration = document.querySelector(".song-duration");
-
-//action buttons
-const buttonRandom = document.querySelector(".button-random");
-
-containerSongs.innerHTML = "";
-
-// display songs
-
-// songs.forEach((song) => {
-//   const html = `
-//     <p class="songlist-songs song-title">${song.title}</p>
-//     <p class="songlist-songs song-artist">${song.artist}</p>
-//     <p class="songlist-songs song-album">${song.album}</p>
-//     <p class="songlist-songs song-duration">${song.duration}</p>
-//     `;
-
-//   containerSongs.insertAdjacentHTML("afterbegin", html);
-// });
-
-//buttons functionality
-
-//random (shuffle)
-buttonRandom.addEventListener("click", (e) => {
-  console.log("clicked");
-  //reload page
-
-  // window.location.reload();
-
-  /* Randomize array in-place using Durstenfeld shuffle algorithm */
-  function shuffleArray(songs) {
-    for (let i = array.length - 1; i > 0; i--) {
-      let j = Math.floor(Math.random() * (i + 1));
-      let temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
-    }
-  }
+// previous song
+previousBtn.addEventListener("click", () => {
+  songIndex < 0 ? (songIndex = songs.length - 1) : songIndex--;
+  loadSongs(songs[songIndex]);
+  playSong();
 });
+
+//next song
+nextBtn.addEventListener("click", () => {
+  songIndex > songs.length - 1 ? (songIndex = 0) : songIndex++;
+  loadSongs(songs[songIndex]);
+  playSong();
+});
+
+/* Randomize array in-place using Durstenfeld shuffle algorithm */
+//   function shuffleArray(songs) {
+//     for (let i = array.length - 1; i > 0; i--) {
+//       let j = Math.floor(Math.random() * (i + 1));
+//       let temp = array[i];
+//       array[i] = array[j];
+//       array[j] = temp;
+//     }
+//   }
+//
 
 // need to add an updateUi function
